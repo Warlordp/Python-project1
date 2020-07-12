e97352c43f6e865650f0db8390470e088073d7a7
Warlordp <pmankani@gmail.com>
2020-07-12 14:28:52 +0530

    Update areaofcir.py

--- areaofcir.py
+++ areaofcir.py
@@ -1,6 +1,6 @@
 print("Type the radius of the circle")
 radius = input(float)
-areainit = 2*(radius)
+areainit = float(radius)*float(radius)
 pi = 3.14
 area = float(pi)*float(areainit)
 print(area)
